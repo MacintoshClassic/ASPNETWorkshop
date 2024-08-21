d45962c6efe13c4d783dc55c559a22e8ccb6266b
Oleksandr Shavchenko <oleksandr.shevchenko@swirlpay.net>
2024-08-21 01:12:52 -0600

    merge

--- obj/Debug/net8.0/Workshop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Workshop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89a799dcd68251966851a95a957844049f57e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54293d87ff65be966bb286aa30f4e90e06cdc086")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
