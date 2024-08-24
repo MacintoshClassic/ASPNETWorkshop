4dd3bf2aa42b2f35fc76afbbfc372f5d653515cc
Oleksandr Shavchenko <oleksandr.shevchenko@swirlpay.net>
2024-08-23 18:51:10 -0600

    OrderStatus fixed

--- Models/OrderStatus.cs
+++ Models/OrderStatus.cs
@@ -12,6 +12,8 @@ namespace Workshop.Models
         [Required]
         public Enums.OrderStatusEnum OrderStatusName { get; set; }
 
+        // value can be added only if value >= 0
+        [Range(0, int.MaxValue, ErrorMessage = "Value must be a positive number")]
         public int QuantityOrdered { get; set; }
     }
 }
