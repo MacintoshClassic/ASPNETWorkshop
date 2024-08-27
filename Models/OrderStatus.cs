fddc1f1fe8697e58e0383bbf9e8cdeb09bbc8563
Oleksandr Shavchenko <oleksandr.shevchenko@swirlpay.net>
2024-08-27 00:45:35 -0600

    OrderStatusChanged

--- Models/OrderStatus.cs
+++ Models/OrderStatus.cs
@@ -12,8 +12,14 @@ namespace Workshop.Models
         [Required]
         public Enums.OrderStatusEnum OrderStatusName { get; set; }
 
+        // based on ID from CarPart table
+        [Required]
+        public Guid CarPartId { get; set; }
+
         // value can be added only if value >= 0
         [Range(0, int.MaxValue, ErrorMessage = "Value must be a positive number")]
         public int QuantityOrdered { get; set; }
+
+        public float PriceTotal { get; set; }
     }
 }
